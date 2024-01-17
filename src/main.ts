fd975883e7677caef331c78464afd7a35173edca
alvmenpal9 <alvaromendozap99@gmail.com>
2024-01-17 18:59:09 -0500

    Changes
    Added cors to the app

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200
+  })
   await app.listen(3000);
 }
 bootstrap();
